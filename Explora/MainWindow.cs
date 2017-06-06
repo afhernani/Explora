30de3fd9c48910f0819e394a460f21d7e9e6868d
hernani Aleman Ferraz <afhernani@hotmail.com>
2017-06-06 22:07:06 +0100

    status initial

---
+++ Explora/MainWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+	string what = string.Empty;
+	protected void OnEntryWhatChanged(object sender, EventArgs e)
+	{
+		what = entryWhat.Text;
+		context_id = statusbar.GetContextId (what);
+		statusbar.Push (context_id, what);
+	}
+	string where = string.Empty;
+	static uint context_id ;
+	protected void OnEntryWhereChanged(object sender, EventArgs e)
+	{
+		where = entryWhere.Text;
+		context_id = statusbar.GetContextId (where);
+		statusbar.Push (context_id, where);
+
+	}
+}
---
+++ Explora/SearcherUi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Explora
+{
+	public class SearcherUi
+	{
+		
+	}
+}
